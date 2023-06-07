93263af0d0db26c81c6154a1c85da93b355f7f4b
Daniel Teles <dan.ribeiro@hotmail.com>
2023-06-07 00:48:21 -0300

    add orderModel.js - create the Order model and define its fields

---
+++ models/orderModel.js
@@ -0,0 +1,35 @@
+// Importing libraries and modules
+const Sequelize = require("sequelize");
+const database = require("../db/db");
+
+// Creating the Order model and defining its fields: customer_id, name, order, address, phone
+const Order = database.define(
+  "order",
+  {
+    customer_id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    order: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  { database, modelName: "order", tableName: "orders" }
+);
+
+module.exports = Order;
